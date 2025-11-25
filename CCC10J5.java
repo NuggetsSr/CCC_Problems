bbf7f0cfc341a6f7ecfb849b7f3713e351417363
NuggetsSr <ericcai1219@gmail.com>
2025-11-25 11:04:30 -0500

    Version 2 (Comments + Better Structure)

--- CCC10J5.java
+++ CCC10J5.java
@@ -1,13 +1,63 @@
 package dmoj;
 
+/*
+ * Author: Eric Cai
+ * Date: Nov 25 2025
+ * Problem: CCC '10 J5 - Knight Hop
+ * Description: BFS graph traversal using queue
+ */
+
 import java.util.*;
 
 public class CCC10J5 {
+	
+	static Queue<int[]> q = new LinkedList<>(); // a queue is used to store coordinates for BFS
+	static HashSet<int[]> visited = new HashSet<int[]>(); // HashSet to keep track of what coords already visited
+	static int[][] movement = {{1,2},{-1,2},{1,-2},{-1,-2},{2,1},{2,-1},{-2,1},{-2,-1}}; 
+	// 2d array of possible movements
+	
+	/*
+	 * Returns an integer value of the shortest amount of moves to move from a initial position to a designated end position.
+	 * @param sX an integer denoting the X value of the start position
+	 * @param sY an integer denoting the Y value of the start position
+	 * @param eX an integer denoting the X value of the end position
+	 * @param eY an integer denoting the Y value of the end position
+	 * @return   an integer denoting the shortest amount of moves for the piece to arrive at the end position
+	 */
+	
+	public static int bfs(int sX, int sY, int eX, int eY) {
+		int numMoves = 0;
+		int[] startCoords = {sX, sY, numMoves};
+		q.add(startCoords);
+		// adding first location to the queue and history to start BFS
+		visited.add(new int[] {sX,sY}); //visited does not store # of moves so program does not get stuck in infinite loop
+		// if visited stores # of moves, then a situation might occur where the algorithm paths to a previously visited position but with a different numMoves.
+		while(!q.isEmpty()) {
+			
+			int[] currPos = q.poll(); // deletes and pulls the front of the queue
+			int currX = currPos[0];
+			int currY = currPos[1];
+			int currMoves = currPos[2];
+			
+			if(currX == eX && currY == eY) {
+				return currMoves;
+			}
+			for(int i = 0; i < 8; i++) {
+				int newX = currX + movement[i][0];
+				int newY = currY + movement[i][1];
+				int[] currentCoords = {newX, newY, currMoves+1};
+				int[] c = {newX,newY};
+				// c is needed to check previously visited location, as visited does not include numMoves
+				if(newX >= 0 && newX < 8 && newY >= 0 && newY < 8 && !visited.contains(c)) {
+					q.add(currentCoords);
+					visited.add(c);
+				}
+			}
+		}
+		return -1;
+	}
+	
 	public static void main(String[] args) {
-		Queue<int[]> q = new LinkedList<>(); // a queue is used to store coordinates for BFS
-		HashSet<int[]> history = new HashSet<int[]>(); // HashSet to keep track of what coords already visited
-		int[][] movement = {{1,2},{-1,2},{1,-2},{-1,-2},{2,1},{2,-1},{-2,1},{-2,-1}}; 
-		// 2d array of possible movements
 		
 		Scanner input = new Scanner(System.in);
 		
@@ -18,33 +68,9 @@ public class CCC10J5 {
 		int endY = input.nextInt()-1;
 		// -1 from input to change to 0-based indexing
 		
-		int numMoves = 0;
+	
 		
-		int[] startCoords = {startX, startY, numMoves};
-		q.add(startCoords);
-		history.add(startCoords);
-		// adding first location to the queue and history to start BFS
-		
-		while(!q.isEmpty()) {
-			if(q.peek()[0] == endX && q.peek()[1] == endY) {
-				System.out.println(q.peek()[2]);
-				return;
-				// each element of q is composed of {x, y, number of steps taken}
-			}
-			for(int i = 0; i < 8; i++) {
-				int currentX = q.peek()[0] + movement[i][0];
-				int currentY = q.peek()[1] + movement[i][1];
-				int[] currentCoords = {currentX,currentY, q.peek()[2]+1};
-				int[] c = {currentX,currentY};
-				// c is needed to check previously visited location
-				if(currentX >= 0 && currentX < 8 && currentY >= 0 && currentY < 8 && !history.contains(c)) {
-					q.add(currentCoords);
-					history.add(c);
-				}
-			}
-			q.remove();
-			//remove the beginning of the queue because it has already been read and it isn't the final location
-		}
+		System.out.println(bfs(startX,startY,endX,endY));
 		
 	}
 
